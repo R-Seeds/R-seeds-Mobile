32344e4b37ac9b37b22c74a128a9be4c7262834f
goal651 <bugiriwilson651@gmail.com>
2025-10-16 21:54:58 +0200

    making project details

--- services/constants.ts
+++ services/constants.ts
@@ -1,6 +1,6 @@
 // API Configuration Constants
 
-const SERVER_URL = 'https://r-seeds-backend-production.up.railway.app'
+const SERVER_URL = 'http://10.12.75.125:5050'
 
 export const API_CONFIG = {
   BASE_URL: SERVER_URL,
