d9f410016332285f89debb1d7c7e76728d2ef789
goal651 <bugiriwilson651@gmail.com>
2025-10-17 06:25:52 +0200

    added tem member to project details

--- app/project/detail.tsx
+++ app/project/detail.tsx
@@ -1,9 +1,17 @@
 import { View, Image, TouchableOpacity, Text, ScrollView } from "react-native";
 import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
-
+import Octicons from '@expo/vector-icons/Octicons';
+import ProjectMember from "@/components/project/projectMember";
 
 export default function ProjectScreen() {
+    const keyFeature = [
+        'Autonomous flight planning via mobile app.',
+        'HD video and thermal imaging cameras.',
+        'Modular payload system (cameras, sprayers, delivery boxes).',
+        'Real-time GPS tracking and geofencing.',
+        'Emergency return-to-home functionality.'
+    ]
     return (
         <View className="flex-1 bg-white items-center">
             <TouchableOpacity className="absolute left-5 top-10 bg-gray-500/80 rounded-full z-10 p-2 w-12 h-12"
@@ -14,8 +22,9 @@ export default function ProjectScreen() {
                 className="w-full h-[60%]"
                 resizeMode="cover"
             />
-            <ScrollView className="py-10 px-4 bg-white bottom-60 rounded-t-[50] flex-1 w-full"
-                contentContainerClassName=" w-full flex-1 gap-y-4 ">
+            <ScrollView className="py-10 px-4 bg-white absolute top-[30%] rounded-t-[50] h-[70%] w-full "
+                contentContainerClassName="w-full  gap-y-4 pb-20"
+                showsVerticalScrollIndicator={false}>
                 <View className="flex-row justify-between items-center">
                     <Text className="font-bold text-xl">Fin Tech</Text>
                     <Text className="text-black bg-teal-100 text-xs border border-teal-500  px-2 py-1 rounded-full">Agriculture</Text>
@@ -39,22 +48,55 @@ export default function ProjectScreen() {
                     navigation, SkyScout can capture real-time data from above, making
                     it valuable for agriculture, logistics, disaster management, and security.
                 </Text>
-                <View className="flex-row gap-x-2">
-                    <View className="flex-1 flex-row gap-x-1 items-center">
-                        <Text className="font-semibold text-teal-500 items-center">
+                <View className="flex-row gap-x-2 items-start">
+                    <View className=" flex-row gap-x-1 items-center">
+                        <Text className="font-bold text-teal-500 items-center">
                             Mission
                         </Text>
                         <Ionicons name="arrow-forward" size={15} color="black" />
                     </View>
-                    <Text className="font-bold">
+                    <Text className=" test-sm flex-1">
                         To provide reliable, versatile, and cost-effective drone
                         solutions that empower communities, businesses, and
                         organizations.
                     </Text>
                 </View>
+                <View className="flex-row gap-x-2 items-start">
+                    <View className=" flex-row gap-x-1 items-center">
+                        <Text className="font-bold text-teal-500 items-center">
+                            Vision
+                        </Text>
+                        <Ionicons name="arrow-forward" size={15} color="black" />
+                    </View>
+                    <Text className=" test-sm flex-1">
+                        To provide reliable, versatile, and cost-effective drone
+                        solutions that empower communities, businesses, and
+                        organizations.
+                    </Text>
+                </View>
+                <View className="items-center gap-y-2">
+                    <Text className="font-bold text-xl text-teal-500">Key Features</Text>
+                    {keyFeature.map((feature, index) => (
+                        <View className="flex-row gap-x-2 items-center" key={index}>
+                            <Octicons name="dot-fill" size={15} color="black" />
+                            <Text className="test-sm flex-1">
+                                {feature}
+                            </Text>
+                        </View>
+                    ))}
+                </View>
+                <View className="items-center gap-y-2">
+                    <Text className="font-bold text-xl text-teal-500 w-full">Team</Text>
+                    <ScrollView horizontal showsHorizontalScrollIndicator={false}
+                    contentContainerClassName="gap-x-4">
+                        <ProjectMember />
+                        <ProjectMember />
+                        <ProjectMember />
+                    </ScrollView>
+                </View>
             </ScrollView>
-            <TouchableOpacity className="absolute bottom-20  bg-teal-500 p-2 rounded-full">
-                <Text className="text-white font-semibold">Contribute Now</Text>
+            <TouchableOpacity className="absolute bottom-20  bg-teal-500/80 py-4 px-8 rounded-full">
+                <Text className="text-white font-semibold text-lg">Contribute Now</Text>
             </TouchableOpacity>
         </View>
     )
